fb5272fff688635501241c81da00747fc4bd3cf0
donpapitoskey <alarcon.juan@correounivalle.edu.co>
2021-02-20 11:28:26 -0500

    spacing created => adding icons and colors

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,7 +1,17 @@
 import React from 'react';
+import {Container, Icon, Sidebar, CoinsContainer} from './styles';
 
 const Header: React.FC = () => {
-  return <div>Header</div>;
+  return (
+    <Container>
+      <Icon>icon</Icon>
+      <Sidebar>
+        <CoinsContainer>more coins</CoinsContainer>
+        <h3>name</h3>
+        <CoinsContainer>Current</CoinsContainer>
+      </Sidebar>
+    </Container>
+  );
 };
 
 export default Header;
--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -1,1 +1,24 @@
+import styled from 'styled-components';
 
+export const Container = styled.div`
+  min-height: 2rem;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  padding: 1rem;
+`;
+
+export const Icon = styled.div``;
+
+export const Sidebar = styled.div`
+  display: flex;
+  flex-direction: row;
+  > * {
+    margin: 0.5rem;
+  }
+  h3 {
+    font-size: 1rem;
+  }
+`;
+
+export const CoinsContainer = styled.div``;
