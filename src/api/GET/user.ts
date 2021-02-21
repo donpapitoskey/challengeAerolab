6719624f69b94fc1262d01ca5cf3942cb861e0db
donpapitoskey <alarcon.juan@correounivalle.edu.co>
2021-02-20 20:05:02 -0500

    adding fetching os user to serversideProps

--- src/api/GET/user.ts
+++ src/api/GET/user.ts
@@ -1,17 +1,16 @@
-import { URL_API } from 'src/utils/constants';
-import { FetchOptions } from 'src/interfaces/fetchOptions';
+import {URL_API} from 'src/utils/constants';
+import {FetchOptions} from 'src/interfaces/fetchOptions';
 
-export const getProducts = () => {
+export const getUser = () => {
   const endpoint = `${URL_API}/user/me`;
 
   const fetchOptions: FetchOptions = {
     method: 'GET',
     headers: {
       'Content-Type': 'application/json',
-      Accept: 'application/json',
-      Authorization: `Bearer ${process.env.AUTH_TOKEN}`
-    }
+      'Accept': 'application/json',
+      'Authorization': `Bearer ${process.env.AUTH_TOKEN}`,
+    },
   };
-
   return fetch(endpoint, fetchOptions);
 };
