afa2cd89da670bfe096b42e3ca07b173a41e6984
donpapitoskey <alarcon.juan@correounivalle.edu.co>
2021-02-21 23:27:39 -0500

    banner checked

--- src/components/Banner/styles.ts
+++ src/components/Banner/styles.ts
@@ -8,11 +8,19 @@ export const Container = styled.div<ContainerProps>`
   background-image: url(${(props) => props.src});
   background-size: cover;
   position: relative;
-  min-height: 60vh;
-  padding: 0 3rem;
+  height: 8rem;
   color: white;
+  padding: 0 1rem;
   h1 {
     position: absolute;
-    bottom: 2rem;
+    bottom: 0rem;
+  }
+  @media (min-width: 650px) {
+    background-size: cover;
+    min-height: 60vh;
+    padding: 0 3rem;
+    h1 {
+      bottom: 2rem;
+    }
   }
 `;
